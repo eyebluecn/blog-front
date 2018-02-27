e4d2131d4564b38c20028ef48b735dbf29e0131a
zicla <ziclax@126.com>
2018-02-27 17:26:08 +0800

    Add multi selection feature in the NbTanks widget.

--- src/backyard/tag/List.vue
+++ src/backyard/tag/List.vue
@@ -1,22 +1,22 @@
 <template>
-	<div class="animated fadeIn">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="pedia-navigation">
-					<span class="item active">标签列表</span>
-				</div>
-			</div>
-		</div>
+  <div class="animated fadeIn">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="pedia-navigation">
+          <span class="item active">标签列表</span>
+        </div>
+      </div>
+    </div>
 
-		<div class="row">
-			<div class="col-lg-8 col-lg-offset-2">
-				<NbFilter :pager="pager" :callback="search">
-					<router-link class="btn btn-primary btn-sm" to="/by/tag/create">
-						<i class="fa fa-plus"></i>
-						创建标签
-					</router-link>
-				</NbFilter>
-			</div>
+    <div class="row">
+      <div class="col-lg-8 col-lg-offset-2">
+        <NbFilter :pager="pager" :callback="search">
+          <router-link class="btn btn-primary btn-sm" to="/by/tag/create">
+            <i class="fa fa-plus"></i>
+            创建标签
+          </router-link>
+        </NbFilter>
+      </div>
       <div class="col-lg-8 col-lg-offset-2">
         <div class="row">
           <div class="col-md-6" v-for="(tag,index) in pager.data">
@@ -28,43 +28,63 @@
       </div>
 
 
+      <div class="col-lg-8 col-lg-offset-2 mt20">
+        <NbPager :pager="pager" :callback="refresh"/>
+      </div>
 
-			<div class="col-lg-8 col-lg-offset-2 mt20">
-				<NbPager :pager="pager" :callback="refresh"/>
-			</div>
 
-		</div>
+      <div class="col-lg-8 col-lg-offset-2 mt20">
+        <h2>NbTank</h2>
 
-	</div>
+        <div>
+          <NbTank :preview="true" :tank="tank"/>
+        </div>
+
+        <h2>NbTanks</h2>
+        <div>
+          <NbTanks :tanks="tanks" :maxNum="3"/>
+        </div>
+
+
+      </div>
+
+    </div>
+
+  </div>
 </template>
 
 <script>
   import NbFilter from '../../common/widget/filter/NbFilter.vue'
   import NbPager from '../../common/widget/NbPager.vue'
-  import { MessageBox, Notification } from 'element-ui'
   import Pager from '../../common/model/base/Pager'
   import Tag from '../../common/model/tag/Tag'
   import TagCell from './widget/TagCell'
+  import NbTank from '../../common/widget/NbTank'
+  import NbTanks from '../../common/widget/NbTanks'
+  import Tank from "../../common/model/tank/Tank";
 
   export default {
-    name: 'list',
-    data () {
+    data() {
       return {
         pager: new Pager(Tag),
-        user: this.$store.state.user
+        user: this.$store.state.user,
+        tank: new Tank("*", false, 1024 * 1024, "单个文件的测试上传"),
+        tanks: []
       }
     },
     components: {
       NbFilter,
       NbPager,
-      TagCell
+      TagCell,
+      NbTank,
+      NbTanks
     },
     methods: {
-      search () {
+      search() {
         this.pager.page = 0
         this.refresh()
       },
-      refresh () {
+      refresh() {
         this.pager.httpFastPage()
       }
     },
@@ -75,7 +95,7 @@
         this.pager.setFilterValue('userUuid', this.user.uuid)
       }
     },
-    mounted () {
+    mounted() {
       this.pager.enableHistory()
       if (!this.pager.getFilterValue('userUuid')) {
         this.pager.setFilterValue('userUuid', this.user.uuid)
--- src/common/widget/NbTanks.vue
+++ src/common/widget/NbTanks.vue
@@ -1,97 +1,99 @@
 <template>
-	<div class="nb-tanks-block">
+  <div class="nb-tanks-block">
 
 
-		<div class="tank-block" v-for="(tank,tankIndex) in tanks">
-			<NbExpanding>
-				<div v-show="tank.procedure === tank.Procedure.FETCHING_UPLOAD_TOKEN">
-					<i class="fa fa-spinner fa-spin fa-fw"></i> 准备上传中...
-				</div>
-			</NbExpanding>
+    <div class="tank-block" v-for="(tank,tankIndex) in tanks">
+      <NbExpanding>
+        <div v-show="tank.procedure === tank.Procedure.FETCHING_UPLOAD_TOKEN">
+          <i class="fa fa-spinner fa-spin fa-fw"></i> 准备上传中...
+        </div>
+      </NbExpanding>
 
 
-			<NbExpanding>
-				<div class="huge-block clearfix"
-				     v-if="tank.procedure === tank.Procedure.UPLOADING">
-					<div class="media">
-						<div class="pull-right">
-							<i class="btn-action f16 fa fa-trash text-danger" @click.stop.prevent="del(tank,tankIndex)"></i>
-						</div>
-						<div class="media-body">{{tank.file.name}}</div>
-					</div>
-					<div class="progress" :class="{'progress-striped active' : tank.procedure === tank.Procedure.UPLOADING}">
-						<div :style="'width: '+(tank.progress*100)+'%'" class="progress-bar progress-bar-primary">
-							<span>已上传 {{(tank.progress * 100).toFixed(1)}}%</span>
-						</div>
-					</div>
-					<div>
-						已上传:{{ (tank.file.size * tank.progress).toFixed(0) | humanFileSize }}/{{ tank.file.size | humanFileSize}} 速度:{{tank.speed | humanFileSize}}/s
+      <NbExpanding>
+        <div class="huge-block clearfix"
+             v-if="tank.procedure === tank.Procedure.UPLOADING">
+          <div class="media">
+            <div class="pull-right">
+              <i class="btn-action f16 fa fa-trash text-danger" @click.stop.prevent="del(tank,tankIndex)"></i>
+            </div>
+            <div class="media-body">{{tank.file.name}}</div>
+          </div>
+          <div class="progress" :class="{'progress-striped active' : tank.procedure === tank.Procedure.UPLOADING}">
+            <div :style="'width: '+(tank.progress*100)+'%'" class="progress-bar progress-bar-primary">
+              <span>已上传 {{(tank.progress * 100).toFixed(1)}}%</span>
+            </div>
+          </div>
+          <div>
+            已上传:{{ (tank.file.size * tank.progress).toFixed(0) | humanFileSize }}/{{ tank.file.size | humanFileSize}}
+            速度:{{tank.speed | humanFileSize}}/s
 
-					</div>
-				</div>
-			</NbExpanding>
+          </div>
+        </div>
+      </NbExpanding>
 
-			<NbExpanding>
-				<div class="tiny-block"
-				     v-if="tank.exist()">
-					<div class="w200 p10 mb10 bg-white br5 border" v-show="tank.publicImgUrl()">
-						<img class="wp100" :src="tank.publicImgUrl()"/>
-					</div>
-					<div>
-						<i class="f16" v-show="tank.fileIcon() && !tank.publicImgUrl()" :class="[tank.fileIcon()]"></i>
-						<span class="f14 black cursor hover-underline" @click.stop.prevent="tank.download()">
+      <NbExpanding>
+        <div class="tiny-block"
+             v-if="tank.exist()">
+          <div class="w200 p10 mb10 bg-white br5 border" v-show="tank.publicImgUrl()">
+            <img class="wp100" :src="tank.publicImgUrl()"/>
+          </div>
+          <div>
+            <i class="f16" v-show="tank.fileIcon() && !tank.publicImgUrl()" :class="[tank.fileIcon()]"></i>
+            <span class="f14 black cursor hover-underline" @click.stop.prevent="tank.download()">
 					{{tank.name}}
 				</span>
-						<span>
+            <span>
 					{{tank.size | humanFileSize}}
 				</span>
-						<span>
+            <span>
 					<i class="btn-action f16 fa fa-download text-success" @click.stop.prevent="tank.download()"></i>
 				</span>
-						<span v-if="edit">
+            <span v-if="edit">
 					<i class="btn-action f16 fa fa-trash text-danger" @click.stop.prevent="del(tank,tankIndex)"></i>
 				</span>
 
-					</div>
-				</div>
-			</NbExpanding>
+          </div>
+        </div>
+      </NbExpanding>
 
-		</div>
+    </div>
 
-		<div
-			v-show="tanks.length < maxNum && edit && templateTank.procedure === templateTank.Procedure.FREE && !templateTank.exist()">
-			<div>
+    <div
+      v-show="tanks.length < maxNum && edit && templateTank.procedure === templateTank.Procedure.FREE && !templateTank.exist()">
+      <div>
 				<span class="btn btn-primary btn-sm btn-file">
 					<slot name="button">
 						<i class="fa fa-folder-open-o"></i>
 						<span>选择文件</span>
 					</slot>
-					<input ref="refFile" type="file" name="avatar" @change.prevent.stop="fileChanged"/>
+					<input ref="refFile" type="file" name="avatar" @change.prevent.stop="fileChanged" multiple/>
 				</span>
-			</div>
-			<div class="mt5 italic" v-if="!templateTank.errorMessage && templateTank.uploadHint">
-				<i class="fa fa-info-circle"></i> {{templateTank.uploadHint}}
-			</div>
-			<div class="mt5 " v-if="templateTank.errorMessage">
-				<i class="fa fa-warning text-danger"></i> <span class="text-danger">{{templateTank.errorMessage}}</span>
-			</div>
-		</div>
+      </div>
+      <div class="mt5 italic" v-if="!templateTank.errorMessage && templateTank.uploadHint">
+        <i class="fa fa-info-circle"></i> {{templateTank.uploadHint}}
+      </div>
+      <div class="mt5 " v-if="templateTank.errorMessage">
+        <i class="fa fa-warning text-danger"></i> <span class="text-danger">{{templateTank.errorMessage}}</span>
+      </div>
+    </div>
 
 
-	</div>
+  </div>
 
 </template>
 
 <script>
 
-  import { startWith, endWith, getExtension, containStr } from '../../common/filter/str'
+
+  import {startWith, endWith, getExtension, containStr} from '../../common/filter/str'
   import Tank from '../../common/model/tank/Tank'
   import $ from 'jquery'
-  import { MessageBox, Notification } from 'element-ui'
+  import {MessageBox, Notification} from 'element-ui'
   import NbExpanding from './NbExpanding.vue'
 
   export default {
-    data () {
+    data() {
       return {}
     },
     props: {
@@ -114,7 +116,8 @@
         type: Tank,
         required: false,
         'default': function () {
-          return new Tank('*', true)
+          //默认模板是所有文件均可上传，私有，最大10M
+          return new Tank('*', true, 1024 * 1024 * 10)
         },
         validator: function (tank) {
           return tank.validateFilter()
@@ -134,47 +137,83 @@
     },
     computed: {},
     methods: {
-      fileChanged () {
+      fileChanged() {
         let that = this
 
-        let tank = new Tank('*', true)
-        tank.render(this.templateTank)
+        let fileDom = this.$refs['refFile']
 
-        tank.file = this.$refs['refFile'].files[0]
-        that.$refs['refFile'].value = ''
-
-        //提前验证，有错误及时显示
-        if (!tank.validate()) {
-          this.templateTank.errorMessage = tank.errorMessage
-          console.error(tank.errorMessage)
+        if (!fileDom.files) {
+          console.error("没有定义文件选择控件 refFile")
           return
-        } else {
-          this.templateTank.errorMessage = null
+        }
+        let selectedLength = fileDom.files.length;
+        if (selectedLength <= 0) {
+          that.templateTank.errorMessage = "没有选择文件"
+          return
         }
 
-        this.tanks.push(tank)
+        if (selectedLength + that.tanks.length > that.maxNum) {
+          that.templateTank.errorMessage = "文件数量超出了限制：" + that.maxNum
+          return
+        }
 
-        tank.httpUpload(function () {
-          if (that.success) {
-            that.success(tank)
+        //提前验证，有错误及时显示
+        for (let i = 0; i < selectedLength; i++) {
+
+          let tank = new Tank('*', true)
+          tank.render(this.templateTank)
+
+          tank.file = fileDom.files[i]
+
+          //提前验证，有错误及时显示
+          if (!tank.validate()) {
+            this.templateTank.errorMessage = tank.errorMessage
+            console.error(tank.errorMessage)
+            return
+          } else {
+            this.templateTank.errorMessage = null
           }
-        }, function () {
-          console.error('上传失败啦')
 
-          //一次检查哪些失败了，失败了的移除掉。
-          for (let i = 0; i < that.tanks.length; i++) {
-            let itemTank = that.tanks[i]
+        }
 
-            if (itemTank.errorMessage) {
-              that.templateTank.errorMessage = itemTank.errorMessage
-              that.tanks.splice(i, 1)
+        //没有错误了再集中进行上传处理。
+        for (let i = 0; i < selectedLength; i++) {
+
+          let tank = new Tank('*', true)
+          tank.render(this.templateTank)
+
+          tank.file = fileDom.files[i]
+
+          this.tanks.push(tank)
+
+          tank.httpUpload(function () {
+            if (that.success) {
+              that.success(tank)
+            }
+          }, function () {
+            console.error('上传失败啦')
+
+            //一次检查哪些失败了，失败了的移除掉。
+            for (let i = 0; i < that.tanks.length; i++) {
+              let itemTank = that.tanks[i]
+
+              if (itemTank.errorMessage) {
+                that.templateTank.errorMessage = itemTank.errorMessage
+                that.tanks.splice(i, 1)
+              }
+
             }
 
-          }
+          })
 
-        })
+        }
+
+
+        fileDom.value = ''
+
       },
-      del (tank, tankIndex) {
+
+      del(tank, tankIndex) {
         let that = this
         MessageBox.confirm('是否删除此文件?', '提示', {
           confirmButtonText: '确定',
@@ -192,7 +231,7 @@
         )
       }
     },
-    mounted () {
+    mounted() {
 
     }
   }
@@ -201,26 +240,26 @@
 
 <style lang="less" rel="stylesheet/less">
 
-	.nb-tanks-block {
+  .nb-tanks-block {
 
-		.tank-block {
+    .tank-block {
 
-			transition-duration: 0.3s;
-			margin: 5px 0;
+      transition-duration: 0.3s;
+      margin: 5px 0;
 
-			.huge-block {
-				background-color: white;
-				border-radius: 5px;
-				border: 1px solid #eeeeee;
-				padding: 10px;
+      .huge-block {
+        background-color: white;
+        border-radius: 5px;
+        border: 1px solid #eeeeee;
+        padding: 10px;
 
-				.progress {
-					margin-bottom: 10px;
-				}
-			}
-		}
+        .progress {
+          margin-bottom: 10px;
+        }
+      }
+    }
 
-	}
+  }
 
 
 </style>
